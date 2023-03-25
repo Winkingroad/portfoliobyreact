4c74b1fe67a7d877d3e9bbd9d46aaccad7a87eb6
Yash Srivastava <yash.srivastava1102@gmail.com>
2023-03-26 03:40:06 +0530

    'portfolio'

---
+++ src/components/About.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 75,
+          scale: 1,
+          speed: 950,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        I am a master of many trades, a true polymath, with a boundless curiosity for the world and all that it contains. I am a technology enthusiast, ever eager to explore the latest advancements in the field, and a skilled web developer, able to bring my ideas to life with code. My knowledge of machine learning is vast and ever-growing, and my background in finance and project management gives me a unique perspective on how to apply these technologies to solve real-world problems.
+
+I am driven by a desire to understand the workings of the universe and to use this knowledge to create something new and beautiful. I take inspiration from the natural world and strive to create technology that is not only functional, but also elegant and intuitive. I believe that true innovation comes from the ability to see the connections between different fields and to apply this knowledge in new and creative ways.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
